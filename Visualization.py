a42f37df1bebc8ce751706d823f02a3a23255d1c
Leonardo Celente <leocelente@gmail.com>
2022-04-03 12:08:11 -0300

    Minor cosmetic changes

--- Visualization.py
+++ Visualization.py
@@ -14,24 +14,24 @@ def Viz(data, time) -> None:
   
   minutes =       time / 60
 
-  plt.figure(figsize=(14, 8))
-  plt.title("Simulation Results")
+  fig = plt.figure(figsize=(14, 8), constrained_layout=True)
+  fig.suptitle('Simulation Results', fontsize=16)
 
   ax = plt.subplot(3,2,1)
   plt.plot(minutes, altitudes, linewidth=2, color="black", label='Altitude ($h$)')
-  plt.legend()
+  plt.legend(loc=0)
   plt.ylabel("Altitude [km]")
   plt.grid()
 
   plt.subplot(3,2,2, sharex=ax)
   plt.plot(minutes, altitudes, linewidth=2, color="black", label="Altitude ($h$)")
-  plt.legend()
+  plt.legend(loc=0)
   plt.ylabel("Altitude [km]")
   plt.grid()
 
   plt.subplot(3,2,3, sharex=ax)
   plt.plot(minutes, speed, linewidth=2, color="orange", label="Ascent Rate ($\dot h$)")
-  plt.legend()
+  plt.legend(loc=0)
   plt.ylabel("Speed [m/s]")
   plt.grid()
   
@@ -40,20 +40,20 @@ def Viz(data, time) -> None:
   plt.plot(minutes, drag, linewidth=2, color="green", label="Drag ($D$)")
   plt.plot(minutes, weights, linewidth=2, color="yellow", label="Weight ($W$)")
   plt.ylabel("Force [N]")
-  plt.legend()
+  plt.legend(loc=0)
   plt.grid()
   plt.xlabel("Time [minutes]")  
 
   plt.subplot(3,2,4, sharex=ax)
   plt.plot(minutes, acceleration, linewidth=2, color="purple", label="Acc. ($\ddot h$)")
   plt.ylabel("Acceleration [$m/s^2$]")
-  plt.legend()
+  plt.legend(loc=0)
   plt.grid()
 
   ax2 = plt.subplot(3,2,6, sharex=ax)
   plt.plot(minutes, volumes, linewidth=2, color="blue", label="Volume ($V_{B}$)")
-  ax2.legend()
   plt.ylabel("Volume [$m^3$]")
+  plt.xlabel("Time [minutes]")
   plt.grid()
   
   ax3 = ax2.twinx()
